45202cc688baabb8edd6deea584a03cd5c6379c2
AntonBrito <tonybrito85@gmail.com>
2017-08-12 11:52:52 -0400

    tic tac toe
    this my new commit, added logic!

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,8 +13,52 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 require('./example')
 
- // function runX () {
- //  $(this).text('X')
- // }
- //
- // $('.item').click('runX')
+
+module.exports = {
+
+}
+
+$(function () {
+
+  const myBoard = []
+  const players = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+  const item = [ '0item', '1item', '2item', '3item', '4item', '5item', '6item', '7item', '8item' ]
+  const myboard = ['', '', '']
+
+  let player = 1
+  // const player2 = o
+  $('td').on('click', function () {
+    if (player === 1) {
+      $(this).html('x')
+    } else {
+      $(this).html('o')
+    }
+    let turn = setPlayer(player)
+    player = turn
+    checkHorizontal()
+  })
+  // switchPlayer
+  function setPlayer(player) {
+    if (player === 1) {
+      return 2
+    } else {
+      return 1
+    }
+  }
+
+  function checkforWin () {
+    //  if check
+  }
+  // function checkDiagun()
+  // function checkVertical()
+  let over
+
+  function checkHorizontal () {
+    if ($('#0item').html() === 'x' && $('#1item').html() === 'x' && $('#2item').html() === 'x') {
+      console.log('x wins')
+
+    }
+
+  }
+
+})
